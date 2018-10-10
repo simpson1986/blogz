c794acf2683f70039cb1c063db828cf78f24a3c6
Chris Simpson <chrissimpson371@gmail.com>
2018-10-10 13:11:36 -0500

    add the table

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:izzy@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.Text)
+    
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+class User(db.model):
