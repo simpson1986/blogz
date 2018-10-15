14139ebe50222b06f78719016fa78ef63538881e
Chris Simpson <chrissimpson371@gmail.com>
2018-10-15 16:21:36 -0500

    finish

--- main.py
+++ main.py
@@ -7,21 +7,40 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:izzy@local
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-class blog(db.Model):
 
+class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.Text)
-    
+    title = db.Column(db.String(300))
+    body = db.Column(db.String(2000))
 
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
+    def __init__(self, name,body):
+        self.title = name
+        self.body =body
+
+
 
 @app.route('/')
-def new_post():
-post ='' 
-    if len(post) == 0:
-        return 'Please add a new post'
+def index():
+    return redirect('/blog')
+@app.route('/newpost',methods=['POST','GET'])
+def add_post():
+    if request.method == 'POST':
+        blog_name = request.form['title']
+        blog_body = request.form['body']
+        new_blog = Blog(blog_name,blog_body)
+        db.session.add(new_blog)
+        db.session.commit()
+        return redirect('/blog')
+    return render_template('newpost.html')
+
+@app.route('/blog', methods=['GET'])
+def blog():
+    blog_post = request.args.get('id')
+
+    if blog_post is None:
+        tasks = Blog.query.all()
     else:
-        return redirect('base.html')
+        tasks = Blog.query.filter_by(id = blog_post)
+    return render_template('blog.html',title="Blog", tasks=tasks)
+
+app.run()
